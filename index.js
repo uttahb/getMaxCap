026d8460a5e7bb4cf157cfeb9b53053cee498042
Jishnu <jishnunbhattathiri@gmail.com>
2023-03-05 12:12:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const handler = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Happy Hacking`}))
+  res.end()
+  
+}
+
+export default handler
